423c7cd387159a5745f14c94a9a8e4bfe62c9855
SantiSB <a.santiago.salas.b@gmail.com>
2024-05-18 13:45:25 -0500

    domain config resend

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,21 +1,21 @@
-import ContactEmail from '@/components/emails/ContactEmail';
-import React from 'react';
-import { Resend } from 'resend';
+import ContactEmail from "@/components/emails/ContactEmail";
+import React from "react";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-    const dataForm = await request.json();
-    try {
-        const data = await resend.emails.send({
-            from: 'Luis Fernando Realtor <onboarding@resend.dev>',
-            to: ['a.santiago.salas.b@gmail.com', 'realtorluisfernando@gmail.com'],
-            subject: 'Nuevo mensaje desde tu sitio web',
-            text: '',
-            react: React.createElement(ContactEmail, dataForm),
-        });
-        return Response.json(data);
-    } catch (error) {
-        return Response.json({ error });
-    }
+  const dataForm = await request.json();
+  try {
+    const data = await resend.emails.send({
+      from: "Luis Fernando Realtor <admin@luisfernandorealtor.com>",
+      to: ["a.santiago.salas.b@gmail.com", "realtorluisfernando@gmail.com"],
+      subject: "Nuevo mensaje desde tu sitio web",
+      text: "",
+      react: React.createElement(ContactEmail, dataForm),
+    });
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error });
+  }
 }
